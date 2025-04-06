62d76f6ca15828f5b68355b786870b822b2e30ac
Mohamed Samy <msamy@novomind.com>
2025-04-06 17:47:01 +0200

    fix icalExpander

--- libs/icalExpander.js
+++ libs/icalExpander.js
@@ -1,11 +1,10 @@
-// @grant       unsafeWindow
 if (typeof WB === "undefined") {
   var WB = {};
 }
 
 if (typeof WB.libs === "undefined") {
-  var WB.libs = {};
+  WB.libs = {};
 }
 
-import __WB_icalExpander from 'https://cdn.jsdelivr.net/npm/ical-expander@3.1.0/+esm'
-WB.libs.icalExpander = __WB_icalExpander;
+let __WB_icalExpander = await import('https://cdn.jsdelivr.net/npm/@realhidden/ical-expander@3.2.0/+esm');
+WB.libs.icalExpander = __WB_icalExpander.default;
